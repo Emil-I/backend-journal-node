6da41c1a560eb3269c3745ee1fb634a13499c05c
_emil <emill.zuball@gmail.com>
2018-05-30 22:32:54 +0300

    dwd

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const express = require('express');
+const session = require('express-session');
 
 // let configPath = path.join(__dirname, 'config', process.env.NODE_ENV + '.json');
-let configPath = path.join(__dirname, '/config/config' + '.json');
+let configPath = path.join(__dirname, `/config/config.json`);
 
 const config = require(configPath);
 
@@ -24,7 +25,11 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.set('port', config.port);
 app.set('config', config);
 
-console.log(app.get('port'));
+console.log(app.get('config').database);
+
+
+// connect to DB
+app.set('db', require('./db'))
 
 
 // TODO Добавить обработчик ошибок, ток вот думаб сюда или в отдельный файл пихнуть
--- db/index.js
+++ db/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+
+let db;
+let url = app.get('config').database.connection;
+let dbName = app.get('config').database.name;
+
+MongoClient.connect(url, (err, client) => {
+  if(err) return console.log(err);
+
+  db = client.db(dbName);
+
+  client.close();
+
+  app.listen(3003, () => {
+    console.log('API started');
+  });
+});
+
+module.exports = db;
